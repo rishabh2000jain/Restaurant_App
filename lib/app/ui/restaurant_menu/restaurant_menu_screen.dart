b600f2e3c339ae3d2d3903e4ed9fcaef1b0af6bd
Rishabh Jain <rishabh.jain@unthinkable.co>
2022-09-18 19:19:21 +0530

    Added Dialog for order placed.

--- lib/app/ui/restaurant_menu/restaurant_menu_screen.dart
+++ lib/app/ui/restaurant_menu/restaurant_menu_screen.dart
@@ -4,7 +4,6 @@ import 'package:restaurant_app/app/bloc/bloc_common_state.dart';
 import 'package:restaurant_app/app/bloc/restaurant_menu/restaurant_menu_bloc.dart';
 import 'package:restaurant_app/app/bloc/restaurant_menu/restaurant_menu_events.dart';
 import 'package:restaurant_app/app/bloc/restaurant_menu/restaurant_menu_states.dart';
-import 'package:restaurant_app/app/models/restaurant_menu/restaurant_menu_response_model.dart';
 import 'package:restaurant_app/app/ui/restaurant_menu/widgets/menu_list.dart';
 import 'package:restaurant_app/resources/strings.dart';
 
@@ -35,105 +34,151 @@ class _RestaurantMenuScreenState extends State<RestaurantMenuScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: BlocConsumer<RestaurantMenuBloc, RestaurantMenuStates>(
-              bloc: _restaurantMenuBloc,
-              listener: (BuildContext context, RestaurantMenuStates state) {
-                if (state is PlaceOrderOrderState) {
-                  if (state.status == Status.completed) {
-                    _restaurantMenuBloc.add(GetRestaurantEvent());
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: BlocConsumer<RestaurantMenuBloc, RestaurantMenuStates>(
+                bloc: _restaurantMenuBloc,
+                listener: (BuildContext context, RestaurantMenuStates state) {
+                  if (state is PlaceOrderOrderState) {
+                    if (state.status == Status.completed) {
+                      _showOrderCompleteDialog();
+                      _restaurantMenuBloc.add(GetRestaurantEvent());
+                    }
                   }
-                }
-              },
+                },
+                buildWhen: (prevState, currState) {
+                  return currState is GetRestaurantMenuState ||
+                      currState is RestaurantMenuInitialState;
+                },
+                builder: (BuildContext context, RestaurantMenuStates state) {
+                  if (state is GetRestaurantMenuState) {
+                    if (state.status == Status.completed) {
+                      return MenuList(
+                        menuCategory: state.data!,
+                      );
+                    } else if (state.status == Status.loading) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else {
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              AppStrings.kSomethingWentWrong,
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w900,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  _restaurantMenuBloc.add(GetRestaurantEvent());
+                                },
+                                child: const Text(AppStrings.kTryAgain))
+                          ],
+                        ),
+                      );
+                    }
+                  }
+                  return const SizedBox();
+                },
+              ),
+            ),
+            BlocBuilder<RestaurantMenuBloc, RestaurantMenuStates>(
               buildWhen: (prevState, currState) {
-                return currState is GetRestaurantMenuState ||
-                    currState is RestaurantMenuInitialState;
+                return currState is RestaurantMenuInitialState ||
+                    currState is DeleteOrderSessionState ||
+                    currState is UpdateCurrentCostState;
               },
               builder: (BuildContext context, RestaurantMenuStates state) {
-                if (state is GetRestaurantMenuState) {
-                  if (state.status == Status.completed) {
-                    return MenuList(
-                      menuCategory: state.data!,
-                    );
-                  } else if (state.status == Status.loading) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else {
-                    return Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                if (state is UpdateCurrentCostState) {
+                  num price = state.data;
+                  return InkWell(
+                    onTap: () {
+                      _restaurantMenuBloc.add(PlaceOrderOrderEvent());
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 65,
+                      margin: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(16)),
+                      child: Stack(
+                        alignment: Alignment.center,
                         children: [
                           Text(
-                            AppStrings.kSomethingWentWrong,
-                            style: TextStyle(
-                              fontSize: 25,
-                                fontWeight: FontWeight.w900,
-                                color: Theme.of(context).primaryColor,),
-                          ),
-                          TextButton(onPressed: (){
-                            _restaurantMenuBloc.add(GetRestaurantEvent());
-                          }, child: const Text(AppStrings.kTryAgain))
-                        ],
-                      ),
-                    );
-                  }
-                }
-                return const SizedBox();
-              },
-            ),
-          ),
-          BlocBuilder<RestaurantMenuBloc, RestaurantMenuStates>(
-            buildWhen: (prevState, currState) {
-              return currState is RestaurantMenuInitialState ||
-                  currState is DeleteOrderSessionState ||
-                  currState is UpdateCurrentCostState;
-            },
-            builder: (BuildContext context, RestaurantMenuStates state) {
-              if (state is UpdateCurrentCostState) {
-                num price = state.data;
-                return InkWell(
-                  onTap: () {
-                    _restaurantMenuBloc.add(PlaceOrderOrderEvent());
-                  },
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 65,
-                    margin: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(16)),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Text(
-                          AppStrings.kPlaceOrder,
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Theme.of(context).colorScheme.secondary,
-                              fontWeight: FontWeight.w700),
-                        ),
-                        Positioned(
-                          right: 20,
-                          child: Text(
-                            '\$${price.toStringAsFixed(2)}',
+                            AppStrings.kPlaceOrder,
                             style: TextStyle(
                                 fontSize: 20,
-                                color: Theme.of(context).colorScheme.secondary),
+                                color: Theme.of(context).colorScheme.secondary,
+                                fontWeight: FontWeight.w700),
                           ),
-                        )
-                      ],
+                          Positioned(
+                            right: 20,
+                            child: Text(
+                              '\$${price.toStringAsFixed(2)}',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color:
+                                      Theme.of(context).colorScheme.secondary),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }
-              return const SizedBox.shrink();
-            },
-          )
-        ],
+                  );
+                }
+                return const SizedBox.shrink();
+              },
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  void _showOrderCompleteDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                AppStrings.kOk,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600),
+              ),
+            ),
+          ],
+          content: Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+            height: 70,
+            child: Column(
+              children: const [
+                Text(
+                  AppStrings.kOrderPlacedSuccess,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/resources/strings.dart
+++ lib/resources/strings.dart
@@ -1,9 +1,11 @@
 class AppStrings{
   static const String kAppName = 'Restaurant App';
   static const String kAdd = 'Add';
+  static const String kOk = 'Ok';
   static const String kBestSeller = 'Best Seller';
   static const String kPlaceOrder = 'Place Order';
   static const String kOutOfStock = 'Out Of Stock';
   static const String kSomethingWentWrong = 'Something Went Wrong';
   static const String kTryAgain = 'Try Again';
+  static const String kOrderPlacedSuccess = 'Your Order Was Placed Successfully';
 }
